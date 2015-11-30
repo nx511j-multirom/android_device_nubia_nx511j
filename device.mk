1f49a5749c2d88497308aa753072e36f376a7b52
Chenxi Mao <chenxi.mao2013@gmail.com>
2015-12-01 06:51:34 +0800

    remove useless keylayout

--- device.mk
+++ device.mk
@@ -292,7 +292,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
     $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
-    $(LOCAL_PATH)/keylayout/goodix-ts.kl:system/usr/keylayout/goodix-ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 # data
