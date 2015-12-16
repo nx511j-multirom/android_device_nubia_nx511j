264994d2c99ce8e695c77d6dbb32a0fb26682e2c
Chenxi Mao <chenxi.mao2013@gmail.com>
2015-12-16 12:37:23 +0800

    bring up mokee

--- mokee.mk
+++ mokee.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/nubia/nx511j/full_nx511j.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := Nubia Z9 mini
-PRODUCT_NAME := cm_nx511j
+PRODUCT_NAME := mk_nx511j
 PRODUCT_MODEL := NX511J
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
